fd4021a9c84af57f53dbc8cbf753a8bbea75aadd
Zee <zzeinabfaragg@gmail.com>
2018-09-11 12:28:43 -0400

    dkljg

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
